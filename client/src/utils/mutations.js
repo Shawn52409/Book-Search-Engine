ba40630aa21fa05c7842f1784f004d3a597d3567
Shawn Miller <shawnsmm@gmail.com>
2022-01-02 22:22:58 -0500

    created the mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: SavedBookInput!) {
+        saveBook(book: $book) {
+            username
+            email
+            bookCount
+            savedBooks {
+               authors
+               description
+               bookId
+               image
+               link
+               title 
+            }
+        }
+    }
+`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: String!) {
+        deleteBook(bookId: $bookId) {
+            username
+            email
+            bookCount
+            savedBooks {
+               authors
+               description
+               bookId
+               image
+               link
+               title 
+            }
+        }
+    }
+`;
